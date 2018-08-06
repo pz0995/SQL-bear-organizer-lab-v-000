fbc7d72da8306642db296fb21614420bbfc40fe4
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-06 03:14:35 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name text,
+    age integer,
+    gender char,
+    color text,
+    temperament text,
+    alive boolean
+     -- primary key(id)
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", "11", "F", "Black", "hot", 1),
+(2,"Rowdy", "15", "F", "Black", "cool", 1),
+(3,"Tabitha", "43", "F", "Tan", "hot", 1),
+(4,"Sergeant Brown", "33", "M", "Brown", "mild", 1),
+(5,"Melissa", "21", "M", "Tan", "medium", 1),
+(6,"Grinch", "21", "M", "Brown", "cool", 1),
+(7,"Wendy", "22", "F", "Tan", "cool", 1),
+(8,null, "11", "F", "Black", "hot", 1);
